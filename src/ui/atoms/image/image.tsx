48c28884ae9887c0d7c9fb7b4fdbf9b32445c9e0
bernardoavsouza <bernardoavsouza@gmail.com>
2024-09-25 03:03:24 -0300

    feat: enhance image component

--- src/ui/atoms/image/image.tsx
+++ src/ui/atoms/image/image.tsx
@@ -1,7 +1,33 @@
-import Img, { ImageProps } from 'next/image';
+import clsx from 'clsx';
+import Img, { ImageProps as ImgProps } from 'next/image';
 
-const Image: React.FC<ImageProps> = ({ ...props }) => {
-  return <Img {...props} />;
+type ImageProps = ImgProps & {
+  size?: 'sm' | 'md' | 'lg';
+  rounded?: boolean | 'full';
+  className?: string;
+};
+
+const Image: React.FC<ImageProps> = ({
+  size = 'md',
+  rounded,
+  className,
+  ...props
+}) => {
+  const isFullRounded = rounded === 'full';
+  const isSm = size === 'sm';
+  const isMd = size === 'md';
+  const isLg = size === 'lg';
+
+  const finalSize = ((isSm && 32) || (isMd && 64) || (isLg && 256)) as number;
+
+  return (
+    <Img
+      {...props}
+      width={finalSize}
+      height={finalSize}
+      className={clsx(isFullRounded && 'rounded-full', className)}
+    />
+  );
 };
 
 export default Image;
--- src/ui/atoms/index.tsx
+++ src/ui/atoms/index.tsx
@@ -2,3 +2,4 @@ export { default as ShouldRender } from './should-render/should-render';
 export { default as Text } from './text/text';
 export { default as Icon } from './icon/icon';
 export { default as TextInput } from './input/text-input/text-input';
+export { default as Image } from './image/image';
