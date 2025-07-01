3a784b8eaa16166783d85f30b765940d61461711
bernardoavsouza <bernardoavsouza@gmail.com>
2025-07-01 17:14:04 -0300

    feat: limits app height and width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <div className="h-screen w-screen overflow-y-auto">{children}</div>
+      </body>
     </html>
   );
 }
